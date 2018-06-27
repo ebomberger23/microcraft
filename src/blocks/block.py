e8514b815839d22b05257d58118970ee3845ed52
ebomberger23 <ebomberger@gmail.com>
2018-06-27 16:53:00 -0400

    stuff

---
+++ src/blocks/block.py
@@ -0,0 +1,15 @@
+# ABC for a block class
+
+class Block():
+    durability = 0
+
+    def right_click(self, world):
+        pass
+        # maybe open inventory GUI or something
+
+    def left_click(self, world):
+        # handle block breaking
+
+    def draw(self, x, y, screen):
+        pass
+        # draw tile onto screen
